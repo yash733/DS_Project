8cc6cbf0439a72d9c14d78466732f33e5b559200
yash733 <yash733622@gmail.com>
2024-03-02 11:17:55 +0530

    2.1commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(name="DS ETE Project",
+      version="0.0.1",
+      description="This is my fist end to end project",
+      author_email="yash733622@gmail.com",
+      author="Yash gupta",
+      packages=find_packages(),
+      install_requires = ["pandas"])
