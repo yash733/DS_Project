95d73c6d5bc548576ed90a8c4663712fb9fe62f6
yash733 <yash733622@gmail.com>
2024-03-06 22:56:54 +0530

    done

--- src/exception.py
+++ src/exception.py
@@ -1,16 +1,17 @@
 import sys
 
 def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
-
+    _  ,     _   ,  exc_tb = error_detail.exc_info()
+# Class Objective Traceback
+    file_name = exc_tb.tb_frame.f_code.co_filename
     error_message="Error in script ({0}) at line ({1}) message as ({2})".format(
-        exc_tb.tb_frame.f_code.co_filename, exc_tb.lineno, str(error))
+        file_name, exc_tb.lineno, str(error))
 
     return error_message
 
 class CustomException(Exception):
     def __init__(self, error_message, error_detail:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message = error_message_detail(error_message,error_detail=error_detail)
 
     def __str__(self):
